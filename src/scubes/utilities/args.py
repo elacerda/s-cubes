ee9d988467b1474bd769773d5f26709ee01ab1f0
Eduardo Alberto Duarte Lacerda <dhubax@gmail.com>
2023-12-07 15:27:34 -0300

    reestructure of parse args procedure

--- src/scubes/utilities/args.py
+++ src/scubes/utilities/args.py
@@ -1,35 +1,20 @@
 import sys
 from os import getcwd
 from shutil import which
-from argparse import ArgumentParser, RawDescriptionHelpFormatter, ArgumentDefaultsHelpFormatter
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
-from .constants import SPLUS_PROG_DESC, WAVE_EFF, DATA_DIR, ZPCORR_DIR, ZP_TABLE
+from .constants import SPLUS_PROG_DESC, SPLUS_ARGS
 from .io import print_level
 
-SPLUS_ARGS = {
-    'redo': ['r', dict(action='store_true', default=False, help='Enable redo mode to overwrite final cubes.')],
-    'clean': ['c', dict(action='store_true', default=False, help='Clean intermediate files after processing.')],
-    'force': ['f', dict(action='store_true', default=False, help='Force overwrite of existing files.')],
-    'bands': ['b', dict(default=list(WAVE_EFF.keys()), nargs='+', help='List of S-PLUS bands (space separated).')],
-    'size': ['l', dict(default=500, type=int, help='Size of the cube in pixels.')],
-    'angsize': ['a', dict(default=50, type=float, help="Galaxy's Angular size in arcsec.")],
-    'work_dir': ['w', dict(default=getcwd(), help='Working directory.')],
-    'output_dir': ['o', dict(default=getcwd(), help='Output directory.')],
-    'sextractor': ['x', dict(default='sex', help='Path to SExtractor executable.')],
-    'class_star': ['p', dict(default=0.25, type=float, help='SExtractor CLASS_STAR parameter for star/galaxy separation.')],
-    'verbose': ['v', dict(action='count', default=0, help='Verbosity level.')],
-    'debug': ['D', dict(action='store_true', default=False, help='Enable debug mode.')],
-    'satur_level': ['S', dict(default=1600.0, type=float, help='Saturation level for the png images.')],
-    'data_dir': ['d', dict(default=DATA_DIR, help='Data directory. All input data should be located here.')],
-    'zpcorr_dir': ['Z', dict(default=ZPCORR_DIR, help='Zero-point correction directory (relative to DATA_DIR).')],
-    'zp_table': ['z', dict(default=ZP_TABLE, help='Zero-point table (relative to DATA_DIR).')],
-    'back_size': ['B', dict(default=64, type=int, help='Background mesh size for SExtractor.')],
-    'detect_thresh': ['T', dict(default=1.1, type=float, help='Detection threshold for SExtractor.')],
-    'username': ['U', dict(default=None, help='S-PLUS Cloud username.')],
-    'password': ['P', dict(default=None, help='S-PLUS Cloud password.')]
-}
-
 class readFileArgumentParser(ArgumentParser):
+    '''
+    A class that extends :class:`argparse.ArgumentParser` to read arguments from file.
+
+    Methods
+    -------
+    convert_arg_line_to_args: yeld's arguments reading a file.
+    '''
+
     def __init__(self, *args, **kwargs):
         super(readFileArgumentParser, self).__init__(*args, **kwargs)
 
@@ -41,30 +26,63 @@ class readFileArgumentParser(ArgumentParser):
                 break
             yield arg
 
-def scubes_parse_arguments(argv):
-    # CREATE PARSER
+def scubes_create_parser():
+    '''
+    Create the parser for the arguments of the `scubes` entry-point console script.
+    It uses two constants defined at `scubes.utilities.constants.py`. The program 
+    description `SPLUS_PROG_DESC` and the  dictionary of arguments `SPLUS_ARGS`.
+
+    It uses :class:`readFileArgumentParser` to include the option to read the 
+    arguments from a file using the prefix @ before the filename::
+
+        scubes @file.args
+
+    Returns
+    -------
+    parser : :class:`argparse.ArgumentParser`
+        The arguments parser.
+    '''
     _formatter = lambda prog: RawDescriptionHelpFormatter(prog, max_help_position=30)
+    
     parser = readFileArgumentParser(fromfile_prefix_chars='@', description=SPLUS_PROG_DESC, formatter_class=_formatter)
 
     for k, v in SPLUS_ARGS.items():
         long_option = k
         short_option, kwargs = v
         option_string = []
+        positional = False
         if short_option != '':
-            option_string.append(f'-{short_option}')
-        option_string.append(f'--{long_option}')
+            if short_option == 'pos':
+                positional = True
+            else:
+                option_string.append(f'-{short_option}')
+        if positional:
+            option_string.append(long_option)
+        else:
+            option_string.append(f'--{long_option}')
         if (long_option != 'verbose') and (kwargs.get('default') is not None):
             _tmp = kwargs['help']
             kwargs['help'] = f'{_tmp} Default value is %(default)s'
         parser.add_argument(*option_string, **kwargs)
 
-    # positional arguments
-    parser.add_argument('tile', metavar='SPLUS_TILE', help='Name of the S-PLUS tile')
-    parser.add_argument('ra', type=str, metavar='RA', help="Galaxy's right ascension")
-    parser.add_argument('dec', type=str, metavar='DEC', help="Galaxy's declination")
-    parser.add_argument('galaxy', metavar='GALAXY_NAME', help="Galaxy's name")
-    parser.add_argument('specz', type=float, metavar='REDSHIFT', help='Spectroscopic or photometric redshift of the galaxy')    
+    return parser
 
+def scubes_parse_arguments(argv):
+    '''
+    Parse the arguments with the parser created by `scubes_create_parser()` in `argv` list of arguments.
+
+    Parameters
+    ----------
+    argv : list
+        List of arguments such as `sys.argv`.
+
+    Returns
+    -------
+    args : :class:`argparse.Namespace`
+        The arguments namespace class attributes.
+    '''
+    # CREATE PARSER
+    parser = scubes_create_parser()
     args = parser.parse_args(args=argv[1:])
     _sex = which(args.sextractor)
     if _sex is None:
--- src/scubes/utilities/constants.py
+++ src/scubes/utilities/constants.py
@@ -1,3 +1,5 @@
+from os import getcwd
+
 from .. import __author__
 
 DATA_DIR = 'data'
@@ -44,4 +46,35 @@ NAMES_CORRESPONDENT = {
     'F378': 'J0378', 'F395': 'J0395','F410': 'J0410', 'F430': 'J0430', 
     'F515': 'J0515', 'F660': 'J0660', 'F861': 'J0861', 
     'U': 'u', 'G': 'g', 'R': 'r', 'I': 'i', 'Z': 'z',
+}
+
+SPLUS_ARGS = {
+    # optional arguments
+    'redo': ['r', dict(action='store_true', default=False, help='Enable redo mode to overwrite final cubes.')],
+    'clean': ['c', dict(action='store_true', default=False, help='Clean intermediate files after processing.')],
+    'force': ['f', dict(action='store_true', default=False, help='Force overwrite of existing files.')],
+    'bands': ['b', dict(default=list(WAVE_EFF.keys()), nargs='+', help='List of S-PLUS bands (space separated).')],
+    'size': ['l', dict(default=500, type=int, help='Size of the cube in pixels.')],
+    'angsize': ['a', dict(default=50, type=float, help="Galaxy's Angular size in arcsec.")],
+    'work_dir': ['w', dict(default=getcwd(), help='Working directory.')],
+    'output_dir': ['o', dict(default=getcwd(), help='Output directory.')],
+    'sextractor': ['x', dict(default='sex', help='Path to SExtractor executable.')],
+    'class_star': ['p', dict(default=0.25, type=float, help='SExtractor CLASS_STAR parameter for star/galaxy separation.')],
+    'verbose': ['v', dict(action='count', default=0, help='Verbosity level.')],
+    'debug': ['D', dict(action='store_true', default=False, help='Enable debug mode.')],
+    'satur_level': ['S', dict(default=1600.0, type=float, help='Saturation level for the png images.')],
+    'data_dir': ['d', dict(default=DATA_DIR, help='Data directory. All input data should be located here.')],
+    'zpcorr_dir': ['Z', dict(default=ZPCORR_DIR, help='Zero-point correction directory (relative to DATA_DIR).')],
+    'zp_table': ['z', dict(default=ZP_TABLE, help='Zero-point table (relative to DATA_DIR).')],
+    'back_size': ['B', dict(default=64, type=int, help='Background mesh size for SExtractor.')],
+    'detect_thresh': ['T', dict(default=1.1, type=float, help='Detection threshold for SExtractor.')],
+    'username': ['U', dict(default=None, help='S-PLUS Cloud username.')],
+    'password': ['P', dict(default=None, help='S-PLUS Cloud password.')],
+
+    # positional arguments
+    'tile': ['pos', dict(metavar='SPLUS_TILE', help='Name of the S-PLUS tile')],
+    'ra': ['pos', dict(metavar='RA', help="Galaxy's right ascension")],
+    'dec': ['pos', dict(metavar='DEC', help="Galaxy's declination")],
+    'galaxy': ['pos', dict(metavar='GALAXY_NAME', help="Galaxy's name")],
+    'specz': ['pos', dict(type=float, metavar='REDSHIFT', help='Spectroscopic or photometric redshift of the galaxy')],
 }
