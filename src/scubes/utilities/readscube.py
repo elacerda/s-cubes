ebaa8dc1d47031929c312a0a0bcec974f226a3f8
Eduardo Alberto Duarte Lacerda <dhubax@gmail.com>
2024-05-26 21:42:51 -0300

    add properties to read_scube()

--- src/scubes/utilities/readscube.py
+++ src/scubes/utilities/readscube.py
@@ -5,6 +5,8 @@ from astropy.io import fits
 from argparse import Namespace
 from copy import deepcopy as copy
 from astropy.visualization import make_lupton_rgb
+from astropy.coordinates import SkyCoord
+from astropy.wcs import WCS
 
 from .io import print_level
 from ..constants import METADATA_NAMES, BANDS
@@ -16,12 +18,99 @@ class tupperware_none(Namespace):
     def __getattr__(self, attr):
         r = self.__dict__.get(attr, None)
         return r
-    
+
+def get_distance(x, y, x0, y0, pa=0.0, ba=1.0):
+    '''
+    Return an image (:class:`numpy.ndarray`)
+    of the distance from the center ``(x0, y0)`` in pixels,
+    assuming a projected disk.
+
+    Parameters
+    ----------
+    x : array
+        X coordinates to get the pixel distances.
+
+    y : array
+        y coordinates to get the pixel distances.
+
+    x0 : float
+        X coordinate of the origin.
+
+    y0 : float
+        Y coordinate of the origin.
+
+    pa : float, optional
+        Position angle in radians, counter-clockwise relative
+        to the positive X axis.
+
+    ba : float, optional
+        Ellipticity, defined as the ratio between the semiminor
+        axis and the semimajor axis (:math:`b/a`).
+
+    Returns
+    -------
+    pixel_distance : array
+        Pixel distances.
+    '''
+    y = np.asarray(y) - y0
+    x = np.asarray(x) - x0
+    x2 = x**2
+    y2 = y**2
+    xy = x * y
+
+    a_b = 1.0/ba
+    cos_th = np.cos(pa)
+    sin_th = np.sin(pa)
+
+    A1 = cos_th ** 2 + a_b ** 2 * sin_th ** 2
+    A2 = -2.0 * cos_th * sin_th * (a_b ** 2 - 1.0)
+    A3 = sin_th ** 2 + a_b ** 2 * cos_th ** 2
+
+    return np.sqrt(A1 * x2 + A2 * xy + A3 * y2)
+
+def get_image_distance(shape, x0, y0, pa=0.0, ba=1.0):
+    '''
+    Return an image (:class:`numpy.ndarray`)
+    of the distance from the center ``(x0, y0)`` in pixels,
+    assuming a projected disk.
+
+    Parameters
+    ----------
+    shape : (float, float)
+        Shape of the image to get the pixel distances.
+
+    x0 : float
+        X coordinate of the origin.
+
+    y0 : float
+        Y coordinate of the origin.
+
+    pa : float, optional
+        Position angle in radians, counter-clockwise relative
+        to the positive X axis. Defaults to ``0.0``.
+
+    ba : float, optional
+        Ellipticity, defined as the ratio between the semiminor
+        axis and the semimajor axis (:math:`b/a`). Defaults to ``1.0``.
+
+    Returns
+    -------
+    pixel_distance : 2-D array
+        Image containing the distances.
+
+    See also
+    --------
+    :func:`get_distance`
+
+    '''
+    y, x = np.indices(shape)
+    return get_distance(x, y, x0, y0, pa, ba)
+
 class read_scube:
     def __init__(self, filename):
         self.filename = filename
         self._read()
-        self.init()
+        self._init()
     
     def _read(self):       
         try:
@@ -30,15 +119,32 @@ class read_scube:
             print_level(f'{self.filename} - file not found')
             sys.exit()
 
-    def init(self):
-        self.cent_pix = int(self.size/2)
-        
-        # MAG
-        pixscale = self.data_header.get('PIXSCALE', 0.55)
-        a = 1/(2.997925e18*3631.0e-23*pixscale**2)
+    def _init_wcs(self):
+        self.wcs = WCS(self.data_header, naxis=2)
+
+    def _init_centre(self):
+        self.cent_coord = SkyCoord(self.ra, self.dec, unit=('deg', 'deg'), frame='icrs')
+        self.x0, self.y0 = self.wcs.world_to_pixel(self.cent_coord)
+        self.i_x0 = int(self.x0)
+        self.i_y0 = int(self.y0)
+
+    def _mag_values(self):
+        a = 1/(2.997925e18*3631.0e-23*self.pixscale**2)
         x = a*(self.flux__lyx*self.pivot_wave[:, np.newaxis, np.newaxis]**2)
         self.mag__lyx = -2.5*np.ma.log10(x)
         self.emag__lyx = (2.5*np.ma.log10(np.exp(1)))*self.eflux__lyx/self.flux__lyx
+
+    def _init(self):
+        self._init_fov_mask()
+        self._init_wcs()
+        self._init_centre()
+        self._mag_values()
+        self.pa, self.ba = 0, 1
+        self.pixel_distance__yx = get_image_distance(self.weimask__yx.shape, x0=self.i_x0, y0=self.i_y0, pa=self.pa, ba=self.ba)
+        self.n_filters = len(self.filters)
+        self.nx = self.data_header['NAXIS1']
+        self.ny = self.data_header['NAXIS2']
+        self.n_filters = self.data_header['NAXIS3']
 
     def lRGB_image(
         self, rgb=('rSDSS', 'gSDSS', 'iSDSS'), rgb_f=(1, 1, 1), 
@@ -154,8 +260,17 @@ class read_scube:
             print_level('Detection file exists.')
 
         _ = maskStars(args=args, detection_image=detection_image, lupton_rgb=_get_lupton_RGB(conn, args, save_img=False), output_dir='.')
+        self.mask_stars_filename = _.filename
+        self.detection_image_filename = _.detection_image
         self.mask_stars_hdul = _.hdul
-        
+        self.detection_image_hdul = _.detection_image_hdul
+
+    def _init_fov_mask(self):
+        f = self._hdulist['DATA'].data
+        ef = self._hdulist['ERRORS'].data
+        wei = self.weimask__lyx
+        self._fov_mask = np.bitwise_or(wei>0, f<=0, ~(np.isfinite(ef)))
+
     @property
     def weimask__lyx(self):
         return np.broadcast_to(self.weimask__yx, (len(self.filters), self.size, self.size))
@@ -195,11 +310,7 @@ class read_scube:
     @property
     def size(self):
         return self.primary_header.get('SIZE', None)
-    
-    @property
-    def x0tile(self):
-        return self.primary_header.get('X0TILE', None)
-    
+       
     @property
     def ra(self):
         return self.primary_header.get('RA', None)
@@ -209,17 +320,25 @@ class read_scube:
         return self.primary_header.get('DEC', None)
 
     @property
+    def x0tile(self):
+        return self.primary_header.get('X0TILE', None)
+
+    @property
     def y0tile(self):
         return self._hdulist[0].header['Y0TILE']
-    
+
+    @property
+    def pixscale(self):
+        return self.data_header.get('PIXSCALE', 0.55)
+
     @property 
     def weimask__yx(self):
         return self._hdulist['WEIMASK'].data
 
     @property 
     def flux__lyx(self):
-        return np.ma.masked_array(self._hdulist['DATA'].data, mask=self.weimask__lyx>0, copy=True)
+        return np.ma.masked_array(self._hdulist['DATA'].data, mask=self._fov_mask, copy=True)
 
     @property 
     def eflux__lyx(self):
-        return np.ma.masked_array(self._hdulist['ERRORS'].data, mask=self.weimask__lyx>0, copy=True)
+        return np.ma.masked_array(self._hdulist['ERRORS'].data, mask=self._fov_mask, copy=True)
