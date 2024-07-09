6c7e7f2f34a678f037fd941d864815597d02d7fe
Eduardo Alberto Duarte Lacerda <dhubax@gmail.com>
2024-07-09 14:29:02 -0300

    update script run_cubes_ml.sh

--- scripts/run_cubes_ml.sh
+++ scripts/run_cubes_ml.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
-workdir="scubes_S20"
-S=20
-mlfile="masterlist.csv"
-
 echo "Enter S-PLUS Cloud credentials"
-read -s -p "Username:" username
+read -p "Username: " username
+read -s -p "Password: " password
 echo ""
-read -s -p "Password:" password
+if [ -z "$username" ] || [ -z $password ]
+then
+    echo "Username and password are mandatory!"
+    exit 1
+fi
+read -p "Size Multiplicator (20): " size_multiplicator
+if [ -z $size_multiplicator ]
+then
+    size_multiplicator=20
+fi
+read -p "Masterlist file (masterlist.csv): " mlfile
+if [ -z "$mlfile" ]
+then
+    mlfile="masterlist.csv"
+fi
+workdir="scubes_S${size_multiplicator}"
+read -p "workdir ($workdir): " workdir_usr
+if [ ! -z $workdir_usr ]
+then
+    workdir=$workdir_usr
+fi
+imgsdir=${workdir}/images
+echo $username
+echo $password
+echo $size_multiplicator
+echo $mlfile
+echo $workdir
+echo $imgsdir
+exit 1
+
+mkdir -p ${imgsdir}
 
 for gal in `tail +2 $mlfile | cut -f1 -d','`
 do 
@@ -14,9 +41,11 @@ do
         of="${tag}_cube.fits"
         if [[ ! -e $of ]]
         then
-				echo "Creating cube for $gal"
-				scubesml -U $username -P $password -w $workdir -S $S -R $gal $mlfile
-				mv ${tag}/${gal}_cube.fits ${workdir}/.
-				rm -rf $tag
+            echo "Creating cube for $gal"
+            scubesml -U $username -P $password -w $workdir -S $size_multiplicator -R $gal $mlfile
+            mv ${tag}/${gal}_cube.fits ${workdir}/.
+            rm -rf $tag
+            splots ${of}
+            mv ${gal}*.png ${imgsdir}/.
         fi
 done
